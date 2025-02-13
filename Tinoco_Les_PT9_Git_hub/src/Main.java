876270a2977c53a971fc59f57dd58d409eb3f2dc
xxProJavaxxNH <lmtinvas48@insgabrielamistral.cat>
2025-02-13 14:12:18 +0100

    2a modificació GitHub.java

--- Tinoco_Les_PT9_Git_hub/src/Main.java
+++ Tinoco_Les_PT9_Git_hub/src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("1a modificació Git");
-		System.out.println(“2a modificació GitHub”); 
+		System.out.println("2a modificació GitHub"); 
 	}
 }
