eec58698e486e9807e9736b301d14c12a0df1939
Usuario <Usuario@DESKTOP-6O5JHH2>
2025-02-13 13:00:08 +0100

    3a modificació

--- Tinoco_Les_PT9_Git_hub/src/Main.java
+++ Tinoco_Les_PT9_Git_hub/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("1a modificació Git");
 		System.out.println("2a modificació GitHub"); 
+		System.out.println("3a modificació Git");
 	}
 }
